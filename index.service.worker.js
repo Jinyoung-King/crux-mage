95080b5b9d4754c79f534667e4e30b3c3089a879
김진영 <kjy9866@cruxdata.co.kr>
2026-06-12 20:01:14 +0900

    deploy v0.34: 적 피해 무한 스케일 + 일시정지 나가기(보상 정산)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781260586|1778381';
+const CACHE_VERSION = '1781261047|1871828';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
