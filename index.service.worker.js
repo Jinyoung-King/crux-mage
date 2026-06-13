4c71ed2fc2d15403e5d605906bfdeecff4566738
김진영 <kjy9866@cruxdata.co.kr>
2026-06-13 20:15:53 +0900

    deploy v0.83

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781348894|2139063';
+const CACHE_VERSION = '1781349338|2229433';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
