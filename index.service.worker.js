b369c8a797d5a8e98b20139fefb223a11fd18dba
김진영 <kjy9866@cruxdata.co.kr>
2026-06-15 08:14:57 +0900

    deploy v1.63 — 메타 개선 2/2(특성 그리드·도감 확장)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781478649|2309016';
+const CACHE_VERSION = '1781478885|2179770';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
