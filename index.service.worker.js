da50a2050a0a6029153130e0ec5637514f0b0d29
김진영 <kjy9866@cruxdata.co.kr>
2026-06-14 23:57:23 +0900

    deploy v1.35 — 메타 시스템 점진 공개

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781447691|2198458';
+const CACHE_VERSION = '1781449032|2155090';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
