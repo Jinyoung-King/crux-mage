f42233c150a68805dcd265c3576ca5ba6af13839
김진영 <kjy9866@cruxdata.co.kr>
2026-06-14 23:35:04 +0900

    deploy v1.34 — 목표(도전 과제) 시스템

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781445693|2081374';
+const CACHE_VERSION = '1781447691|2198458';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
