f5349f83b8505b2553306ead40fec0a984b2edf0
김진영 <kjy9866@cruxdata.co.kr>
2026-06-15 14:18:35 +0900

    Deploy v1.78 (홈 네비 집 아이콘)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781499835|2210163';
+const CACHE_VERSION = '1781500674|1981851';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
