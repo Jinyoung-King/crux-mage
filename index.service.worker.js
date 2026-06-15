cd1a5e401fe8ba7810896000c99919611719cc7f
김진영 <kjy9866@cruxdata.co.kr>
2026-06-16 01:48:36 +0900

    Deploy v2.6 — 상점 건너뛰기 패널/스폰 버그 수정

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781540737|1965358';
+const CACHE_VERSION = '1781542096|1878914';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
