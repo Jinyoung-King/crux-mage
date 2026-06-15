761094f16507b8e56af48435da4d339e641ff401
김진영 <kjy9866@cruxdata.co.kr>
2026-06-16 07:31:56 +0900

    Deploy v2.9 — 스킬 시전명 팝업 제거

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781542804|2068474';
+const CACHE_VERSION = '1781562692|1876538';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
