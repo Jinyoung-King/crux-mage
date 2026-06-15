bb2877e6f592fae95b0a711f870ab5bc2aa2af13
김진영 <kjy9866@cruxdata.co.kr>
2026-06-15 16:51:45 +0900

    Deploy v1.82 (히트스톱 제거 — 끊김 수정)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781508042|2086428';
+const CACHE_VERSION = '1781509887|1914288';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
