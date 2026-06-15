b9404daf783e64eeb8fc6fe2a83c185f844ae8fa
김진영 <kjy9866@cruxdata.co.kr>
2026-06-16 01:15:17 +0900

    Deploy v2.4 — 신규 스킬 3종 + 비행체 버프

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781538518|1869803';
+const CACHE_VERSION = '1781540095|1877162';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
