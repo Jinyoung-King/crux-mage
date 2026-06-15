6e1402657e5559036fc299ffafcd3d976738e5bb
김진영 <kjy9866@cruxdata.co.kr>
2026-06-16 00:38:30 +0900

    Deploy v2.2 — 스킬 카드 텍스트 침범 실수정

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781536906|1904491';
+const CACHE_VERSION = '1781537893|1985779';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
