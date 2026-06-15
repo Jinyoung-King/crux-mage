e0dd827d3b3c7ffb0c490481c5a2571cfe7c4e87
김진영 <kjy9866@cruxdata.co.kr>
2026-06-15 23:25:32 +0900

    Deploy v1.97 (제단 희귀드래프트 + 스킬 진화 표시)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781532214|1847461';
+const CACHE_VERSION = '1781533513|1821892';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
