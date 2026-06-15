6e328cffeef4223da99bf77d23a8e492c2907980
김진영 <kjy9866@cruxdata.co.kr>
2026-06-15 23:41:09 +0900

    Deploy v1.98 (부여 수치·배너 홈전용·특성 2종)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781533513|1821892';
+const CACHE_VERSION = '1781534445|1908876';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
