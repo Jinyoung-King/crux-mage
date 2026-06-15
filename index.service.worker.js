a11544e760eda8c2080edf1d0d9d4f454e763a2e
김진영 <kjy9866@cruxdata.co.kr>
2026-06-15 22:59:15 +0900

    Deploy v1.95 (균열 자동선택 + 슬롯5)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781529396|2353376';
+const CACHE_VERSION = '1781531938|1824318';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
