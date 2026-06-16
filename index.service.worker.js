944932ae5ddf34a44f76fcfd36375159b57aada9
김진영 <kjy9866@cruxdata.co.kr>
2026-06-16 23:33:10 +0900

    deploy v3.21 — 스킬 적중 스파크

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781619368|1868467';
+const CACHE_VERSION = '1781620373|1874807';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
