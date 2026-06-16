ec345c8a6ae6c5bd55391c1bf9600f7ab939d1dd
김진영 <kjy9866@cruxdata.co.kr>
2026-06-16 13:58:12 +0900

    Deploy v3.8 — 외부 CC0 FX 물·목

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781584793|2025764';
+const CACHE_VERSION = '1781585890|2039083';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
