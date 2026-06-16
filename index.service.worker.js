69addd69f5a17d678b7150d83fd4e829ad7911c9
김진영 <kjy9866@cruxdata.co.kr>
2026-06-16 10:07:21 +0900

    Deploy v3.3 — 유성 픽셀 폭발(아트 업그레이드 샘플)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781569386|2197023';
+const CACHE_VERSION = '1781572020|2108770';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
