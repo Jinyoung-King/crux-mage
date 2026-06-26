663ccc2088fdf4d13dc972126b6adaebc8c92285
Jinyoung-King <Jinyoung-King@users.noreply.github.com>
2026-06-26 10:51:42 +0000

    deploy: c0690052019f62dc4b5fd324a21270722a3171b1

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782467208|3394774';
+const CACHE_VERSION = '1782471099|3217758';
 /** @type {string} */
 const CACHE_PREFIX = 'crux-mage-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
